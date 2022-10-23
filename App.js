91b72ed78e3bc6b8718fb5bcc5588983bf31d9a0
malmz90 <70096644+malmz90@users.noreply.github.com>
2022-10-23 19:22:57 +0200

    test request to backend

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
+import { useEffect } from 'react';
+import styles from './styles';
+import 'expo-dev-menu';
 
 export default function App() {
+	const [welcome, setWelcome] = useState('');
+	console.log(welcome);
+
+	const getAllItems = () =>
+		fetch('http://localhost:4000/welcome', {
+			credentials: 'include',
+		})
+			.then((res) => res.text())
+			.catch((error) => console.log('error', error));
+
+	useEffect(() => {
+		let canceled = false;
+		getAllItems().then((data) => {
+			if (canceled || !data) {
+				return;
+			}
+			setWelcome(data);
+		});
+		return () => {
+			canceled = true;
+		};
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			<Text>Yo!</Text>
+			<Text>{welcome}!</Text>
+
 			<StatusBar style="auto" />
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
